50834568bed4db049cc5896d3abbd61c7ac1ea93
Hammad-Shaikh1 <mhammadhhs@gmail.com>
2023-03-25 16:12:18 +0500

    changes

---
+++ MiniProject2/login.php
@@ -0,0 +1,98 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+
+<?php include 'style.php';
+?>
+</head>
+
+<body>
+
+<?php
+
+include 'connect.php';
+
+if(isset($_POST['submit'])){
+    $Email=$_POST['Email'];
+    $Password=$_POST['Password'];
+
+    $Email_search="select * from users where Email='$Email'";
+ 
+    $query=mysqli_query($con,$Email_search);
+
+    $Email_count=mysqli_num_rows($query);
+
+    if($Email_count){
+        $Email_Pass=mysqli_fetch_assoc($query);
+
+       $db_Pass= $Email_Pass['Password'];
+	   
+	   $_SESSION['YourName'] =$Email_Pass['YourName'];
+	   
+      /*  $Pass_decode=password_verify($Password,$db_Pass); */
+
+       if($db_Pass){
+		   ?>
+           <script>
+           alert("Successfully login");
+           </script>
+		   <?php   
+		   
+           header('location:home.php');
+		      
+		 }
+    else{
+         echo"Password Incorrect";
+             }
+    }
+	else{
+        echo"invalid email";
+	}
+         
+}
+
+?>
+
+
+
+<div class="wrapper">
+    <h1>Login Now</h1>
+
+<form action=""  method="post">
+    <input type="email" name="Email" placeholder="Email" required>
+    <input type="password" name="Password" placeholder="Password" required>
+    
+    
+    <button type="submit" name="submit">Login</button>
+</form>    
+
+
+<div class="member">
+    Not Have an Account? <a href="signup.php">SignUp Here</a>
+</div>
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+<!-- <div class="terms">
+    <input type="checkbox" id="checkbox">
+    <label for="checkbox"> I argee to these term and condition</label>
+  
+</div> -->
